d257767754dc708865807dde78f5813749195017
marre421 <marcus.nordin421@hotmail.com>
2021-01-20 10:38:34 +0100

    Add test

---
+++ tests/MatchSymbolsTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Contracts\RandomizerContract;
+use App\Services\BoardService;
+use App\Services\RandomService;
+use Mockery\MockInterface;
+
+class MatchSymbolsTest extends TestCase
+{
+    /**
+     * Added test to show how I would write test, obviously more tests would be required here
+     */
+    public function testBetIsCorrectOnBigWin()
+    {
+        $this->app->instance(
+            RandomizerContract::class,
+            Mockery::mock(RandomService::class, function (MockInterface $mock) {
+                $mock->shouldReceive('getRandom')->andReturn(collect(['J']));
+            })
+        );
+
+        $boardService = BoardService::make()->generate();
+
+        $board = $boardService->getBoard()->bet([[0, 3, 6, 9, 12]], 100);
+
+        $this->assertEquals(1000, $board->getWin());
+    }
+}
